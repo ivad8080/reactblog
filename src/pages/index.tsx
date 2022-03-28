01b3584545bfc6efb30801e61584cc02ede39580
Davi SAS <ivad8080@gmail.com>
2022-03-28 05:14:59 -0300

    Estilos globais do app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
@@ -7,8 +6,8 @@ export default function Home() {
       <Head>
         <title>react.blog | Home</title>
       </Head>
-      <h1 className={styles.title}>
-        Hello <span>Word</span>
+      <h1>
+        Hello Word
       </h1>
     </>
   )
