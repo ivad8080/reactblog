0eec751c3ced1785b67950532441155d96bc5569
Davi SAS <ivad8080@gmail.com>
2022-03-28 04:29:41 -0300

    Title dinamico por pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head'
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Test <span>Word</span></h1>
+    <>
+      <Head>
+        <title>react.blog | Home</title>
+      </Head>
+      <h1 className={styles.title}>
+        Hello <span>Word</span>
+      </h1>
+    </>
   )
 }
