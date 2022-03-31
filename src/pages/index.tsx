1ea40b352d868bd5416cec9e3f2be0eb647d97d9
Davi SAS <ivad8080@gmail.com>
2022-03-31 00:41:15 -0300

    Pagina Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
 import Head from 'next/head'
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>react.blog | Home</title>
       </Head>
-      <h1>
-        Hello Word
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>🤗 Olá, visitante</span>
+          <h1>Tudo sobre a biblioteca <span>React</span></h1>
+          <p>
+            Apoie nosso blog e receba acesso completo <br />
+            <span>por R$9,90</span>
+          </p>
+        </section>
+        <img src="/images/avatar.png" alt="Dude coding" />
+      </main>
     </>
   )
 }
