228926501a94e47e0336e4f5fb88af484c1f9eb8
Davi SAS <ivad8080@gmail.com>
2022-04-07 21:07:25 -0300

    Salvar dados auth github no fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github'
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -15,5 +17,23 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-  secret: process.env.NEXT_PUBLIC_SECRET
+  secret: process.env.NEXT_PUBLIC_SECRET,
+  callbacks: {
+    async signIn({ user, account, profile}) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email }}
+          )
+        )
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    }
+  }
 })
